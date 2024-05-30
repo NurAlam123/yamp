9a77e8d5e8703b70ea7ec6ef5307ca32e6d2839e
Nur Alam <nuralam.rsc@gmail.com>
2024-05-31 01:10:31 +0600

    MODIFIED: version

--- yamp_py/__init__.py
+++ yamp_py/__init__.py
@@ -3,6 +3,6 @@ __title__ = "YAMP"
 __sub_title__ = "Yet Another Music Player"
 __description__ = "An online music player that no one asked for."
 
-__version__ = "v0.1.0"
+__version__ = "v0.1.2"
 
 __author__ = "Nur Alam"
